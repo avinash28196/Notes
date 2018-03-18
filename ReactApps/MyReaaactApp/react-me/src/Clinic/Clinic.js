3459fa17c12b589a208dfa4fdf1c60dd5ab3e9ee
avinash28196 <avinash28196@gmail.com>
2018-03-18 09:42:26 +0530

    React app basics

---
+++ ReactApps/MyReaaactApp/react-me/src/Clinic/Clinic.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const clinic = (props) => {
+    return  <div>
+            <p> Clinic name is: {props.name} and Doctor Name is: {props.doc} </p>
+            </div>
+};
+
+export default clinic;
