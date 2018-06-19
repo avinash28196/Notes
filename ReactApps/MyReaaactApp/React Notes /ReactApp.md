e0e4aa28f77188413a1c11d53ad30a3af8c1ab84
avinash28196 <avinash28196@gmail.com>
2018-06-19 11:43:39 +0530

    Deleted ReactApps

--- ReactApps/MyReaaactApp/React Notes /ReactApp.md
+++
@@ -1,7 +0,0 @@
-# Create React App
-
-`npm install -g create-react-app`</br>
-`create-react-app my-app`</br>
-`cd my-app`</br>
-`npm start`</br> 
-
--- ReactApps/MyReaaactApp/react-me/src/Clinic/Clinic.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const clinic = (props) => {
-    return  <div>
-            <p> Clinic name is: {props.name} and Doctor Name is: {props.doc} </p>
-            </div>
-};
-
-export default clinic;
